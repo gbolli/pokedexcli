6fbc9bce3df60f825807b5a766b73c2754bec4c8
Gord Bolli <gord@bolli.ca>
2025-01-28 12:05:33 -0800

    create scanner, take input, clean and return first word

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	
+	scanner := bufio.NewScanner(os.Stdin)
+	
+	for {
+
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		clean := cleanInput(input)
+
+		if len(clean) == 0 { 
+			fmt.Print("No input recieved")
+			continue
+		}
+		
+	}
 }
 
 func cleanInput(text string) []string {
